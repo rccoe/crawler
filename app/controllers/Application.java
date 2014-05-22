8519b161fc204f21239ee52b72704d57d04e0558
rccoe <robert.coe.4@gmail.com>
2014-05-22 01:07:03 -0400

    Validated url, now to implement crawl and display

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,14 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Check;
+import play.data.validation.CheckWith;
 import play.mvc.*;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 
 import models.*;
@@ -11,8 +17,57 @@ public class Application extends Controller {
 
     public static void index() {
 
-        List<Website> oldSites = Website.find("order by crawledAt").fetch(5);
+        List<Website> oldSites = Website.find("order by crawledAt desc").fetch(5);
         render(oldSites);
     }
 
+    public static void crawlWebsite(@CheckWith(UrlExistenceValidator.class) String url) {
+
+        if(validation.hasErrors()) {
+            params.flash(); // add http parameters to the flash scope
+            validation.keep(); // keep the errors for the next request
+            index();
+        }
+        Website website = Website.findOrCreate(url);
+        show(website.id);
+    }
+
+    public static void show(Long id) {
+        Website website = Website.findById(id);
+        render(website);
+    }
+
+
+    static class UrlExistenceValidator extends Check {
+        @Override
+        public boolean isSatisfied(Object model, Object urlObj) {
+            if (!(urlObj instanceof String)) {
+                return false;
+            }
+
+            final String urlString = (String) urlObj;
+            try {
+                URL url;
+                try {
+                    url = new URL(urlString);
+                }
+                catch (MalformedURLException muex) {
+                    url = new URL("http://" + urlString);
+                }
+                final HttpURLConnection huc =  (HttpURLConnection) url.openConnection();
+                huc.setRequestMethod("HEAD");
+                huc.connect();
+                final int code = huc.getResponseCode();
+                setMessage("url.responseCode", Integer.toString(code) );
+                return code != 404;
+            }
+
+            catch (IOException ioex) {
+                setMessage("Problem connecting to URL", ioex.getMessage() );
+                return false;
+            }
+
+
+        }
+    }
 }
--- app/models/Website.java
+++ app/models/Website.java
@@ -1,25 +1,39 @@
 package models;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 import javax.persistence.*;
 
+import play.data.validation.*;
 import play.db.jpa.*;
 
 @Entity
 public class Website extends Model {
 
     public String url;
+
     public boolean isCrawled;
     public Date crawledAt;
 
     @OneToMany(mappedBy = "website", cascade = CascadeType.ALL)
     public List<Link> links;
 
-    public Website(String url) {
+    private Website(String url) {
         this.url = url;
         this.isCrawled = false;
         this.links = new ArrayList<Link>();
         this.crawledAt = new Date();
+    }
+
+    public static Website findOrCreate(String url) {
+        Website website = Website.find("byUrl", url).first();
+        if (website == null) {
+            website = new Website(url).save();
+        }
+        return website;
     }
 
     public Link addLink(String path) {
@@ -35,6 +49,8 @@ public class Website extends Model {
         this.save();
         return this;
     }
+
+
 
 
 }
